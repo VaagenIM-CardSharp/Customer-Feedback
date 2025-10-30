4c1f81d27647f61adf31e9c92d2cbe5bb31b405e
mixqoz <unknownmixqo@gmail.com>
2025-10-30 10:25:55 +0100

    Comment section/Rating & Styles

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 
 app = Flask(__name__)
 
-
 file = "feedback.json"
 
 try:
@@ -15,25 +14,34 @@ except:
         json.dump(feedback, f)
 
 @app.route("/")
-def home():
-    with open(file, "r") as f:
-        data = json.load(f)
+def index():
+    f = open(file, "r")
+    data = json.load(f)
+    f.close()
     return render_template("feedback.html", feedbacks=data)
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    rating = request.form["rating"]
+    rating = request.form.get("rating")
+    comment = request.form.get("comment")
 
-    if not rating:
-        return redirect("/")  # do nothing, just go back to home page
-    
-    with open(file, "r") as f:
-        data = json.load(f)
-    data.append({"rating": int(rating)})
-    with open(file, "w") as f:
-        json.dump(data, f)
+    if rating == "" or rating is None:
+        return redirect("/")
+
+    f = open(file, "r")
+    data = json.load(f)
+    f.close()
+
+    data.append({
+        "rating": int(rating),
+        "comment": comment
+    })
+
+    f = open(file, "w")
+    json.dump(data, f)
+    f.close()
+
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
